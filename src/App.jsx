0bb74bc917d90d8f48b20f3d1e9697944c1113c9
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-30 07:51:56 +0800

    Subject : Body

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import './App.css'
 
 function App() {
   
-  const title = "WElcome to the new blog"
+  const title = "Welcome to the new blog"
+  const likes = "50"
 
   return (
     <div className='App'>
       <div className='content'>
-        <h1>App Component</h1>
+        <h1>{title}</h1>
       </div>
     </div>
   )
