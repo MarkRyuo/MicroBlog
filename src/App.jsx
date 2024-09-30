fdd414798c3b841afbbb1a629f53732638f22788
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-30 08:25:24 +0800

    Subject : Body

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,7 @@ function App() {
   return (
     <div className='App'>
       <div className='content'>
-        <h1>{title}</h1>
-        <p>Liked {likes} </p>
-
-        <p>{10}</p>
-        <p>{"Hello NInjas"}</p>
-        <p>{ [1, 2, 3, 4] }</p>
-        <p>{ Math.random()* 20}</p>
-
-        <a href={link}>google</a>
+        
       </div>
     </div>
   )
