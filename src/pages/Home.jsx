6969399889d41cedb47acb6bb896c96f21bb619f
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-30 14:30:42 +0800

    Subject : Body

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title={"All Blogs"}/>
+            <BlogList blogs={blogs} title={"All Blogs"}/>  {/* Child - blogs={blogs} Passing in the child */}
 
         </div>
     );
