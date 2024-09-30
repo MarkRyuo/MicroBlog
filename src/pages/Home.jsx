7d1d91a89bcb1db3e986220c7aeb457ff8618637
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-30 10:43:51 +0800

    Subject : Body

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,12 +7,13 @@ const Home = () => {
     const name = "Moda" ;
 
     const handleClick = () => {
-
+        
     }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name}</p>
             <button type="button" onClick={handleClick}>Click Me</button>
         </div>
     );
