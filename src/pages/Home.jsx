aae415e4558f19728e451ac8f626e457c630f2a1
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-30 10:31:14 +0800

    Subject : Body

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     }
 
     const handleClickAgain = (name, e) => { 
-        console.log("Hello " + name, e) ;
+        console.log("Hello " + name, e.target) ;
 
 
     }
@@ -16,7 +16,7 @@ const Home = () => {
         <div className="home">
             <h2>Homepage</h2>
             <button type="button" onClick={handleClick}>Click Me</button>
-            <button type="button" onClick={() => handleClickAgain("Jhon Mark")}>Click me Again</button>
+            <button type="button" onClick={(e) => handleClickAgain("Jhon Mark" + e)}>Click me Again</button>
         </div>
     );
 }
